c75b1f3f631c5c5584b8b2b261a0b6bca43d87d9
da1c <axda1c21@gmail.com>
2020-03-09 17:19:44 +0900

    Update index.js

--- WebProject/assets/index.js
+++ WebProject/assets/index.js
@@ -47,8 +47,8 @@ console.log("9");
   // ライト追加
   sceneManager.AddObj(light);
 console.log("10");
-  for (let x_idx = 0; x_idx < 10; x_idx++) {
-    for (let y_idx = 0; y_idx < 10; y_idx++) {
+  for (let x_idx = 0; x_idx < 1; x_idx++) {
+    for (let y_idx = 0; y_idx < 1; y_idx++) {
       obj = new Obj3D();
       obj.SetMesh(CreateMesh(400, 400, 400));
       obj.SetPos( x_idx * 15, y_idx * 15, 0 );
