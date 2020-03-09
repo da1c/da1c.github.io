a67618128101613ceadb129fae1b1a11fb3c4904
da1c <axda1c21@gmail.com>
2020-03-09 16:31:49 +0900

    Update BoxMesh.js

--- WebProject/assets/js/test/BoxMesh.js
+++ WebProject/assets/js/test/BoxMesh.js
@@ -3,7 +3,7 @@ function CreateMesh( x, y, z){
 
   // 箱を作成
   geometry = new THREE.BoxGeometry(x, y, z);
-  material = new THREE.MeshBasicMaterial({
+  material = new THREE.MeshNormalMaterial({
     color: 0x0000ff
   });
 
